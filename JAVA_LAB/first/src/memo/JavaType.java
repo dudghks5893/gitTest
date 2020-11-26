5718a3b1398b6618c39f2d42d55f73211111ac1b
dudghks <dudghks2814@naver.com>
2020-11-27 00:44:56 +0900

    메모,코드업 문제 풀이

---
+++ JAVA_LAB/first/src/memo/JavaType.java
@@ -0,0 +1,35 @@
+package memo;
+
+public class JavaType {
+
+	public static void main(String[] args) {
+		
+		//	public = 접근 제어자
+		//  static = 메모리
+		/*  static 변수들 : Static 변수와 static 메소드는 
+			Static 메모리 영역에 존재하므로 객체가 생성되기 이전에 이미 할당이 되어 있습니다. 
+			그렇기 때문에 객체의 생성없이 바로 접근(사용)할 수 있습니다. 
+		*/
+		//	Wrapper Class
+		/*
+		 	기본형   대응 래퍼 클래스
+			byte     Byte
+			short    Short
+			int      Integer
+			long     Long
+			float    Float
+			double   Double
+			char     Char
+			boolean  Boolean
+		 */
+		// 메소드&생성자&객체 등등(String a,double b) = 매개변수
+		// 메소드&생성자&객체 등등("안녕",2.13) = 인자
+		// a = "안녕"; b = 2.13 		=     인자
+		// char c = 'D'  			=	  변수 선언
+		// int[] a = {1,2,3,4};     = 	  배열
+		// {1,2,3,4};               = 	  원소(원자형)
+		
+		
+	}
+
+}
